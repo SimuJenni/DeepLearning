ab8cc5e809732e0bcd592f35c059e6e887db71a6
Simon Jenni <simujenni@students.unibe.ch>
2016-05-10 22:26:55 +0200

    Started assignment 2

--- Stanford/assignment2/cs231n/layers.py
+++ Stanford/assignment2/cs231n/layers.py
@@ -21,10 +21,13 @@ def affine_forward(x, w, b):
   """
   out = None
   #############################################################################
-  # TODO: Implement the affine forward pass. Store the result in out. You     #
+  # Implement the affine forward pass. Store the result in out. You           #
   # will need to reshape the input into rows.                                 #
   #############################################################################
-  pass
+  N = x.shape[0]
+  D = np.prod(x.shape[1:])
+  X = np.reshape(x, (N,D))
+  out = X.dot(w) + b
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
@@ -50,9 +53,16 @@ def affine_backward(dout, cache):
   x, w, b = cache
   dx, dw, db = None, None, None
   #############################################################################
-  # TODO: Implement the affine backward pass.                                 #
+  #  Implement the affine backward pass.                                      #
   #############################################################################
-  pass
+  N = x.shape[0]
+  D = np.prod(x.shape[1:])
+  dx = dout.dot(w.T)
+  dx = np.reshape(dx, x.shape)
+  X = np.reshape(x, (N,D))
+  dw = X.T.dot(dout)
+  db = dout.T.dot(np.ones(N))
+
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
@@ -72,9 +82,10 @@ def relu_forward(x):
   """
   out = None
   #############################################################################
-  # TODO: Implement the ReLU forward pass.                                    #
+  #  Implement the ReLU forward pass.                                         #
   #############################################################################
-  pass
+  out = np.copy(x)
+  out[x <= 0.0] = 0.0  # ReLU
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
@@ -95,9 +106,10 @@ def relu_backward(dout, cache):
   """
   dx, x = None, cache
   #############################################################################
-  # TODO: Implement the ReLU backward pass.                                   #
+  # Implement the ReLU backward pass.                                         #
   #############################################################################
-  pass
+  dx = np.copy(dout)
+  dx[x <= 0.0] = 0.0
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
